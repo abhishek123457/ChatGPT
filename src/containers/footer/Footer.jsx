f1b03b80f982fa6c86ae8528a928d77b7ef3d48d
Abhishek Kumar <101491128+abhishek123457@users.noreply.github.com>
2023-07-04 15:52:14 +0530

    Add files via upload

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import gpt3Logo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+  <div className="gpt3__footer section__padding">
+    <div className="gpt3__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="gpt3__footer-btn">
+      <button class="btn">Request Early Access </button>
+    </div>
+
+    <div className="gpt3__footer-links">
+      <div className="gpt3__footer-links_logo">
+        <img src={gpt3Logo} alt="gpt3_logo" />
+        <p>KIIT , <br /> All Rights Reserved</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="gpt3__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>KIIT Bhubaneswar</p>
+        <p>8653838100</p>
+        <p>2005773@kiit.ac.in</p>
+      </div>
+    </div>
+
+    <div className="gpt3__footer-copyright">
+      <p>@2023 GPT-3. All rights reserved.</p>
+    </div>
+  </div>
+);
+
+export default Footer;
